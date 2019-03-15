f17a660f7d7b6f331475b871d77160d441641f53
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-03-15 12:48:33 -0500

    Displaying stored messages

--- imports/ui/Chat.jsx
+++ imports/ui/Chat.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-export default class Chat extends Component {
+import { withTracker } from "meteor/react-meteor-data";
+import { Messages } from "../api/messages.js";
+
+
+class Chat extends Component {
+  renderMessages() {
+    return this.props.messages.map( m =>
+      <div key={m._id}>{m.message}</div>
+    );
+  }
+
   render() {
+    console.log("Messages", this.props.messages);
     return (
       <div>
         <h2>Messages</h2>
-        <div className="messsges" />
+        <div className="messsges">
+          { this.renderMessages() }
+        </div>
         <input type="text" placeholder="Enter your message" />
       </div>
     );
   }
 }
+
+Chat.propTypes = {
+  messages : PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+export default withTracker(() => {
+  return {
+    messages: Messages.find({}).fetch()
+  };
+}
+)(Chat);
--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from "meteor/meteor";
 
+import "../imports/api/messages.js";
 
 Meteor.startup(() => {
 
