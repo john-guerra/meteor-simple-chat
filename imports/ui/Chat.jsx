559e1d9a7274d5e2556d8c718f737203be11d448
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-03-15 12:54:49 -0500

    Inserting comments

--- imports/ui/Chat.jsx
+++ imports/ui/Chat.jsx
@@ -4,12 +4,43 @@ import PropTypes from "prop-types";
 import { withTracker } from "meteor/react-meteor-data";
 import { Messages } from "../api/messages.js";
 
-
 class Chat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: ""
+    };
+  }
+
   renderMessages() {
-    return this.props.messages.map( m =>
-      <div key={m._id}>{m.message}</div>
-    );
+    return this.props.messages.map(m => <div key={m._id}>{m.message}</div>);
+  }
+
+  onChange(evt) {
+    console.log("change", evt.target.value);
+    this.setState({
+      message: evt.target.value
+    });
+  }
+
+  onKey(evt) {
+    if (evt.key === "Enter") {
+      Messages.insert({
+        message: this.state.message
+      }, (err, res) => {
+        if (err) {
+          alert("There was error inserting check the console");
+          console.log(err);
+          return;
+        }
+
+        console.log("Message inserted", res);
+        this.setState({
+          message: ""
+        });
+      });
+    }
   }
 
   render() {
@@ -17,22 +48,25 @@ class Chat extends Component {
     return (
       <div>
         <h2>Messages</h2>
-        <div className="messsges">
-          { this.renderMessages() }
-        </div>
-        <input type="text" placeholder="Enter your message" />
+        <div className="messsges">{this.renderMessages()}</div>
+        <input
+          type="text"
+          placeholder="Enter your message"
+          value={this.state.message}
+          onChange={ this.onChange.bind(this) }
+          onKeyPress = { this.onKey.bind(this) }
+        />
       </div>
     );
   }
 }
 
 Chat.propTypes = {
-  messages : PropTypes.arrayOf(PropTypes.object).isRequired
+  messages: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default withTracker(() => {
   return {
     messages: Messages.find({}).fetch()
   };
-}
-)(Chat);
+})(Chat);
